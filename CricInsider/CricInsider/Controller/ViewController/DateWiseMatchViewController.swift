0720e7d0f22ab773557c2abcce6e3e96287be620
MD SAKIBUL ALAM UTCHAS <63851422+msa-utchas@users.noreply.github.com>
2023-02-22 20:32:45 +0600

    xib completed

--- CricInsider/CricInsider/Controller/ViewController/DateWiseMatchViewController.swift
+++ CricInsider/CricInsider/Controller/ViewController/DateWiseMatchViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class DateWiseMatchViewController: UIViewController {
     
+    @IBOutlet weak var backView: UIView!
     
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var tableViewMatchList: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        backView.addShadow()
+        tableViewMatchList.dataSource = self
+        tableViewMatchList.delegate = self
+        let nib = UINib(nibName: MatchesTableViewCell.identifier, bundle: nil)
+        tableViewMatchList.register(nib, forCellReuseIdentifier: MatchesTableViewCell.identifier)
        
     }
 
 
+}
+extension DateWiseMatchViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewMatchList.dequeueReusableCell(withIdentifier: MatchesTableViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    
 }
---
+++ CricInsider/CricInsider/ViewModel/DateWiseMatchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DateWiseMatchViewModel.swift
+//  CricInsider
+//
+//  Created by Md. Sakibul Alam Utchas on 22/2/23.
+//
+
+import Foundation

436dec333b27e70ce3f17c517f9ef67ca5706ccd
MD SAKIBUL ALAM UTCHAS <63851422+msa-utchas@users.noreply.github.com>
2023-02-22 16:53:44 +0600

    push

---
+++ CricInsider/CricInsider/Controller/ViewController/DateWiseMatchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DateWiseMatchViewController.swift
+//  CricInsider
+//
+//  Created by BJIT on 22/2/23.
+//
+
+import UIKit
+
+class DateWiseMatchViewController: UIViewController {
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var tableViewMatchList: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+}
